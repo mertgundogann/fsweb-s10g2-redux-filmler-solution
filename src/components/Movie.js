e02b1a66a7a5d498d62376eb077f7b5598a204aa
Gökhan <gn.ozdemir@gmail.com>
2023-11-22 09:42:48 +0300

    delete movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteMovie } from "../actions/movieActions";
+
 const Movie = (props) => {
   const { id } = useParams();
   const { push } = useHistory();
+  const dispatch = useDispatch();
 
   // const movies = [];
   const movies = useSelector((state) => state.movies);
   const movie = movies.find((movie) => movie.id === Number(id));
+
+  const deleteHandler = () => {
+    dispatch(deleteMovie(id));
+    push("/movies");
+  };
 
   return (
     <div className="bg-white rounded-md shadow flex-1">
@@ -37,7 +45,11 @@ const Movie = (props) => {
         </div>
       </div>
       <div className="px-5 py-3 border-t border-zinc-200 flex justify-end gap-2">
-        <button type="button" className="myButton bg-red-600 hover:bg-red-500">
+        <button
+          onClick={deleteHandler}
+          type="button"
+          className="myButton bg-red-600 hover:bg-red-500"
+        >
           Sil
         </button>
         <button className="myButton bg-blue-600 hover:bg-blue-500 ">
