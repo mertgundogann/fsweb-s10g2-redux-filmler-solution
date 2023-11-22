cade7cd49ac191a6e62dba52aa7535ed4f8e375b
Gökhan <gn.ozdemir@gmail.com>
2023-11-22 09:27:22 +0300

    setups initial reducer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-import { legacy_createStore as createStore } from 'redux';
-import { Provider } from 'react-redux';
-import reducer from './reducers';
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={createStore(reducer)}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
